6b17e3a285edb4577ba576f38fe2225a663c207e
Elvin Camalzade <e.camalzade@itstep.edu.az>
2022-08-30 10:27:56 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+           var result= MessageBox.Show("WPF","Clicked to inner button",MessageBoxButton.OKCancel,MessageBoxImage.Warning);
+            if(result == MessageBoxResult.OK)
+            {
+                btn.Background = Brushes.Orange;
+            }
+
+        }
+        public int Count { get; set; }
+        private void btn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            btn.Content = (++Count).ToString();
+        }
+
+        private void StackPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+         //   btn.Content = Mouse.GetPosition(Application.Current.MainWindow).X;
+        }
+    }
+}
